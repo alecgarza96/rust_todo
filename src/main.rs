8d78a288c8c197192899b06beb05d25a464c050f
Alec Garza <alecgarza96@gmail.com>
2025-06-27 04:25:25 -0500

    task and priority input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::i32;
+use std::io;
 
 struct Args {
     task: String,
@@ -8,5 +9,27 @@ struct Args {
 
 fn main() {
     println!("Enter a to do list item:");
+    let mut enter_task = 1;
+    while enter_task == 1 {
+        let mut task = Args {
+            task: String::new(),
+            priority: 0,
+            due_date: "".to_string()
+        };
+        let mut input = String::new();
+        println!("Enter Task Name:");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to Read Line");
+        task.task = input.trim().to_string();
+        input.clear();
+        println!("Enter Priority (1-5):");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to Read Line");
+        task.priority = input.trim().parse().expect("Enter a valid number");
+        input.clear();
+        println!("Enter Due Date (dd/mm/yyyy):");
+    }
 
 }
